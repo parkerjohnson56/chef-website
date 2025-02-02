a22cd0c556d820ae374edde167f3e9e48d6b571e
pjohns80 <parkeriscool83@gmail.com>
2025-02-01 18:53:44 -0600

    Hero section + nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Italiana } from "next/font/google";
 import "./globals.css";
 import Nav from '@/components/nav/Nav'
 
@@ -11,6 +12,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const italiana = Italiana({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-italiana',
 });
 
 export const metadata: Metadata = {
@@ -26,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${italiana.variable} antialiased`}
       >
         <Nav />
         {children}
